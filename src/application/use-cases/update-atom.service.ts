65e0caca44761a436b8af30f3d2bd078b5a08003
Quibylix <barraza.fredimanuel@gmail.com>
2026-03-15 19:38:15 -0600

    feat: add UpdateAtomService to change atom element

---
+++ src/application/use-cases/update-atom.service.ts
@@ -0,0 +1,28 @@
+import { ResultAsync } from "neverthrow";
+import { EntityId } from "../../domain/base/entity.base";
+import { ElementSymbol } from "../../domain/value-objects/elements";
+import { ApplicationService } from "../base/application-service.base";
+import { MoleculeRepository } from "../../domain/repositories/molecule-repository";
+
+export class UpdateAtomCommand {
+  constructor(
+    public readonly moleculeId: EntityId,
+    public readonly atomId: EntityId,
+    public readonly elementSymbol: ElementSymbol,
+  ) {}
+}
+
+export class UpdateAtomService implements ApplicationService<
+  UpdateAtomCommand,
+  void
+> {
+  constructor(private repository: MoleculeRepository) {}
+
+  public execute(command: UpdateAtomCommand): ResultAsync<void, Error> {
+    return this.repository.findById(command.moleculeId).andThen((molecule) => {
+      return molecule
+        .updateAtomElement(command.atomId, command.elementSymbol)
+        .asyncAndThen(() => this.repository.save(molecule));
+    });
+  }
+}
