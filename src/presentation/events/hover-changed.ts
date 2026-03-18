59a976795e14ed3e0ba94b109a55d726284d94e1
Quibylix <barraza.fredimanuel@gmail.com>
2026-03-17 20:14:13 -0600

    refactor: update HoverChanged to support atom and bond hover

--- src/presentation/events/hover-changed.ts
+++ src/presentation/events/hover-changed.ts
@@ -1,7 +1,20 @@
 import { EntityId } from "../../domain/base/entity.base";
 import { PresentationEvent } from "../base/presentation-events";
 
+export type HoverTarget =
+  | { type: "atom"; atomId: EntityId }
+  | { type: "bond"; atomIds: [EntityId, EntityId] }
+  | null;
+
 export class HoverChanged implements PresentationEvent {
   public readonly occurredOn: Date = new Date();
-  constructor(public readonly atomId: EntityId | null) {}
+  constructor(public readonly target: HoverTarget) {}
+
+  get atomId(): EntityId | null {
+    return this.target?.type === "atom" ? this.target.atomId : null;
+  }
+
+  get bondAtomIds(): [EntityId, EntityId] | null {
+    return this.target?.type === "bond" ? this.target.atomIds : null;
+  }
 }
