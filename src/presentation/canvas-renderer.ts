d07d3d9a1e345d119b8c05dff9af27467e69e912
Quibylix <barraza.fredimanuel@gmail.com>
2026-03-17 20:14:22 -0600

    feat: add bond highlighting to Scene and renderer

--- src/presentation/canvas-renderer.ts
+++ src/presentation/canvas-renderer.ts
@@ -1,4 +1,12 @@
+import { BondType } from "../domain/entities/bond";
 import { ScreenPoint } from "./screen-point";
+
+const BOND_LINE_WIDTH = 2;
+const BOND_LINE_COLOR = "#333";
+const BOND_HIGHLIGHT_WIDTH = 8;
+const BOND_HIGHLIGHT_COLOR = "#defa";
+const DOUBLE_BOND_SPACING = 4;
+const TRIPLE_BOND_SPACING = 4;
 
 export class CanvasRenderer {
   private ctx: CanvasRenderingContext2D;
@@ -56,13 +64,108 @@ export class CanvasRenderer {
     this.ctx.fillText(symbol, x, y);
   }
 
-  public drawBond(start: ScreenPoint, end: ScreenPoint) {
+  public drawBond(
+    start: ScreenPoint,
+    end: ScreenPoint,
+    bondType: BondType = BondType.Single,
+    highlighted: boolean = false,
+  ) {
+    if (highlighted) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(start.x, start.y);
+      this.ctx.lineTo(end.x, end.y);
+      this.ctx.strokeStyle = BOND_HIGHLIGHT_COLOR;
+      this.ctx.lineWidth = BOND_HIGHLIGHT_WIDTH;
+      this.ctx.stroke();
+      this.ctx.closePath();
+    }
+
+    switch (bondType) {
+      case BondType.Single:
+        this.drawSingleBond(start, end);
+        break;
+      case BondType.Double:
+        this.drawDoubleBond(start, end);
+        break;
+      case BondType.Triple:
+        this.drawTripleBond(start, end);
+        break;
+    }
+  }
+
+  private drawSingleBond(start: ScreenPoint, end: ScreenPoint) {
     this.ctx.beginPath();
     this.ctx.moveTo(start.x, start.y);
     this.ctx.lineTo(end.x, end.y);
-    this.ctx.strokeStyle = "#333";
-    this.ctx.lineWidth = 2;
+    this.ctx.strokeStyle = BOND_LINE_COLOR;
+    this.ctx.lineWidth = BOND_LINE_WIDTH;
     this.ctx.stroke();
     this.ctx.closePath();
+  }
+
+  private drawDoubleBond(start: ScreenPoint, end: ScreenPoint) {
+    const offset = this.getPerpendicularOffset(start, end, DOUBLE_BOND_SPACING);
+
+    this.ctx.strokeStyle = BOND_LINE_COLOR;
+    this.ctx.lineWidth = BOND_LINE_WIDTH;
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(start.x + offset.x, start.y + offset.y);
+    this.ctx.lineTo(end.x + offset.x, end.y + offset.y);
+    this.ctx.stroke();
+    this.ctx.closePath();
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(start.x - offset.x, start.y - offset.y);
+    this.ctx.lineTo(end.x - offset.x, end.y - offset.y);
+    this.ctx.stroke();
+    this.ctx.closePath();
+  }
+
+  private drawTripleBond(start: ScreenPoint, end: ScreenPoint) {
+    const offset = this.getPerpendicularOffset(start, end, TRIPLE_BOND_SPACING);
+
+    this.ctx.strokeStyle = BOND_LINE_COLOR;
+    this.ctx.lineWidth = BOND_LINE_WIDTH;
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(start.x, start.y);
+    this.ctx.lineTo(end.x, end.y);
+    this.ctx.stroke();
+    this.ctx.closePath();
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(start.x + offset.x, start.y + offset.y);
+    this.ctx.lineTo(end.x + offset.x, end.y + offset.y);
+    this.ctx.stroke();
+    this.ctx.closePath();
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(start.x - offset.x, start.y - offset.y);
+    this.ctx.lineTo(end.x - offset.x, end.y - offset.y);
+    this.ctx.stroke();
+    this.ctx.closePath();
+  }
+
+  private getPerpendicularOffset(
+    start: ScreenPoint,
+    end: ScreenPoint,
+    distance: number,
+  ): { x: number; y: number } {
+    const dx = end.x - start.x;
+    const dy = end.y - start.y;
+    const length = Math.sqrt(dx * dx + dy * dy);
+
+    if (length === 0) {
+      return { x: 0, y: 0 };
+    }
+
+    const perpX = -dy / length;
+    const perpY = dx / length;
+
+    return {
+      x: perpX * distance,
+      y: perpY * distance,
+    };
   }
 }
--- src/presentation/scene.ts
+++ src/presentation/scene.ts
@@ -2,8 +2,10 @@ import { EntityId } from "../domain/base/entity.base";
 
 export class Scene {
   public hoveredAtomId: EntityId | null = null;
+  public hoveredBondAtomIds: [EntityId, EntityId] | null = null;
 
   public clear(): void {
     this.hoveredAtomId = null;
+    this.hoveredBondAtomIds = null;
   }
 }
