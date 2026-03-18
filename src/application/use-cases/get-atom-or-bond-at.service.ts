74540789d6b3badd897edaeaf1931a522d75ccbf
Quibylix <barraza.fredimanuel@gmail.com>
2026-03-17 20:13:56 -0600

    feat: add GetAtomOrBondAtService and UpdateBondTypeService

---
+++ src/application/use-cases/get-atom-or-bond-at.service.ts
@@ -0,0 +1,52 @@
+import { ResultAsync, okAsync } from "neverthrow";
+import { MoleculeRepository } from "../../domain/repositories/molecule-repository";
+import { EntityId } from "../../domain/base/entity.base";
+import { BondType } from "../../domain/entities/bond";
+
+export type AtomOrBondDTO =
+  | { type: "atom"; atomId: EntityId }
+  | { type: "bond"; atomIds: [EntityId, EntityId]; bondType: BondType };
+
+export class GetAtomOrBondAtQuery {
+  constructor(
+    public readonly moleculeId: EntityId,
+    public readonly x: number,
+    public readonly y: number,
+    public readonly atomRadius: number = 20,
+    public readonly bondThreshold: number = 10,
+  ) {}
+}
+
+export class GetAtomOrBondAtService {
+  constructor(private repository: MoleculeRepository) {}
+
+  public execute(
+    query: GetAtomOrBondAtQuery,
+  ): ResultAsync<AtomOrBondDTO | null, Error> {
+    return this.repository.findById(query.moleculeId).andThen((molecule) => {
+      const result = molecule.getAtomOrBondAt(
+        query.x,
+        query.y,
+        query.atomRadius,
+        query.bondThreshold,
+      );
+
+      if (!result) {
+        return okAsync(null);
+      }
+
+      if (result.type === "atom") {
+        return okAsync({
+          type: "atom" as const,
+          atomId: result.item.id,
+        });
+      }
+
+      return okAsync({
+        type: "bond" as const,
+        atomIds: result.item.atomIds,
+        bondType: result.item.type,
+      });
+    });
+  }
+}
---
+++ src/application/use-cases/update-bond-type.service.ts
@@ -0,0 +1,29 @@
+import { ResultAsync } from "neverthrow";
+import { EntityId } from "../../domain/base/entity.base";
+import { BondType } from "../../domain/entities/bond";
+import { ApplicationService } from "../base/application-service.base";
+import { MoleculeRepository } from "../../domain/repositories/molecule-repository";
+
+export class UpdateBondTypeCommand {
+  constructor(
+    public readonly moleculeId: EntityId,
+    public readonly atomAId: EntityId,
+    public readonly atomBId: EntityId,
+    public readonly newType: BondType,
+  ) {}
+}
+
+export class UpdateBondTypeService implements ApplicationService<
+  UpdateBondTypeCommand,
+  void
+> {
+  constructor(private repository: MoleculeRepository) {}
+
+  public execute(command: UpdateBondTypeCommand): ResultAsync<void, Error> {
+    return this.repository.findById(command.moleculeId).andThen((molecule) => {
+      return molecule
+        .updateBondType(command.atomAId, command.atomBId, command.newType)
+        .asyncAndThen(() => this.repository.save(molecule));
+    });
+  }
+}
