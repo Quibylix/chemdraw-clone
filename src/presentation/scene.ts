d49b34e543e3e838c53124edcf04e0a153a20c1c
Quibylix <barraza.fredimanuel@gmail.com>
2026-03-19 17:37:30 -0600

    feat: convert Scene to use preact signals

--- src/presentation/scene.ts
+++ src/presentation/scene.ts
@@ -1,11 +1,34 @@
+import { BondType } from "../chemistry/domain/entities/bond";
 import { EntityId } from "../shared/domain/base/entity.base";
+import { signal, Signal } from "@preact/signals";
+
+export type AtomDTO = {
+  id: string;
+  symbol: string;
+  x: number;
+  y: number;
+};
+
+export type BondDTO = {
+  atomAId: string;
+  atomBId: string;
+  type: BondType;
+};
 
 export class Scene {
-  public hoveredAtomId: EntityId | null = null;
-  public hoveredBondAtomIds: [EntityId, EntityId] | null = null;
+  public atoms: Signal<AtomDTO[]> = signal([]);
+  public bonds: Signal<BondDTO[]> = signal([]);
+  public hoveredAtomId: Signal<EntityId | null> = signal(null);
+  public hoveredBondAtomIds: Signal<[EntityId, EntityId] | null> = signal(null);
+
+  public insertAtom(atom: AtomDTO) {
+    this.atoms.value = [...this.atoms.value, atom];
+  }
 
   public clear(): void {
-    this.hoveredAtomId = null;
-    this.hoveredBondAtomIds = null;
+    this.atoms.value = [];
+    this.bonds.value = [];
+    this.hoveredAtomId.value = null;
+    this.hoveredBondAtomIds.value = null;
   }
 }
