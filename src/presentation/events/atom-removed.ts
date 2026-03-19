870d0571fbf53bdf4b91c4c2f52493196341453d
Quibylix <barraza.fredimanuel@gmail.com>
2026-03-19 17:36:47 -0600

    refactor: remove presentation event system

--- src/presentation/events/atom-removed.ts
+++
@@ -1,7 +0,0 @@
-import { PresentationEvent } from "../base/presentation-events";
-
-export class AtomRemoved implements PresentationEvent {
-  public readonly occurredOn: Date = new Date();
-
-  constructor(public readonly atomId: string) {}
-}
--- src/presentation/events/atom-updated.ts
+++
@@ -1,7 +0,0 @@
-import { PresentationEvent } from "../base/presentation-events";
-
-export class AtomUpdated implements PresentationEvent {
-  public readonly occurredOn: Date = new Date();
-
-  constructor(public readonly atomId: string) {}
-}
